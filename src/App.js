081c41052d179c00857ccdae22815d5f996b17de
Peerachai.k <idcode358@gmail.com>
2021-11-22 10:10:13 +0700

    installed react-routeer-dom to route page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import Header from './components/headers/header';
-import SideBar from './components/navigations/sidebar';
+import React from 'react';
+
+import MainNavbar from './components/navbars/main-navbar';
+import SubNavbar from './components/navbars/sub-navbar';
+import Home from './Pages/home';
+import CreateItem from './Pages/create-item';
+import DeleteItem from './Pages/delete-item';
+import EditItem from './Pages/edit-item';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  // Redirect,
+} from "react-router-dom";
 
 import './App.css';
+import ListItem from './Pages/list-item';
 
-const itemList = [{
+const routes = [{
   path: '/create',
   label: 'Create Item',
   component: 'CreateItem'
 },{
-  path: '/update',
-  label: 'Update Item',
-  component: 'UpdateItem'
+  path: '/edit',
+  label: 'Edit Item',
+  component: 'EditItem'
 },{
   path: '/delete',
   label: 'Delete Item',
   component: 'DeleteItem'
 },{
-  path: '/home',
+  path: '/users',
   label: 'ItemList',
   component: 'ItemList'
 }];
@@ -25,8 +39,17 @@ function App() {
 
   return (
     <div className="App">
-      <Header label={'seekster'} />
-      <SideBar itemList={itemList} />
+      <BrowserRouter>      
+        <MainNavbar label={'seekster'} />
+        <SubNavbar routes={routes} />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/create" element={<CreateItem />} />
+          <Route path="/delete" element={<DeleteItem />} />
+          <Route path="/edit" element={<EditItem />} />
+          <Route path="/users" element={<ListItem />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
